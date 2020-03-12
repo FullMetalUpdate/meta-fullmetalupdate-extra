6bce747962edc5edb75a00d9539127ba96fc08be
antznin <antonin.godard@telecom-paristech.fr>
2020-03-12 10:36:10 +0100

    Increase u-boot environment size
     * Increase env size in update_common.h
     * Update the fw_env.config to match the size of regular and redundant
       environment

--- dynamic-layers/freescale-layer/recipes-bsp/u-boot/imx8mqevk/update_common.h
+++ dynamic-layers/freescale-layer/recipes-bsp/u-boot/imx8mqevk/update_common.h
@@ -1,7 +1,7 @@
 #ifndef U_BOOT_UPDATE_COMMON_H
 
 #define CONFIG_ENV_OFFSET		(1 * 1024)
-#define CONFIG_ENV_SIZE			(3 * 1024)
+#define CONFIG_ENV_SIZE			(10 * 1024)
 #define CONFIG_SYS_MMC_ENV_DEV		0
 #define CONFIG_CMD_IMPORTENV
 
